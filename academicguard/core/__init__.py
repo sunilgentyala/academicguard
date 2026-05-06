67d4983df1a6643eeaebe00550711d4f91d93909
Sunil Gentyala <146994248+sunilgentyala@users.noreply.github.com>
2026-05-05 23:26:33 -0500

    Delete academicguard directory

--- academicguard/core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Core package init."""
-from academicguard.core.document import Document, DocumentSection
-from academicguard.core.report import AnalysisReport, Finding, ModuleResult
-
-__all__ = ["Document", "DocumentSection", "AnalysisReport", "Finding", "ModuleResult"]
--- academicguard/integrations/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Integrations package init."""
-from academicguard.integrations.external import (
-    ZeroGPTAdapter,
-    GPTZeroAdapter,
-    GrammarlyAdapter,
-    check_available_services,
-    ENV_SETUP_GUIDE,
-)
-__all__ = ["ZeroGPTAdapter", "GPTZeroAdapter", "GrammarlyAdapter", "check_available_services", "ENV_SETUP_GUIDE"]
